a6bfc132ef145d0a8497bc6feb7b3ddcf2df621a
Lewis Hendy <lewishendy26@gmail.com>
2026-06-01 22:29:35 +0100

    dde fake data generation library to csv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use clap::{Parser, Subcommand};
+use fake::Fake;
+use fake::faker::address::en::*;
+use fake::faker::internet::en::*;
+use fake::faker::name::en::*;
+use fake::uuid::UUIDv4;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io;
@@ -45,11 +50,19 @@ fn write_csv(path: &str, lines: u32, delim: String) -> Result<(), io::Error> {
     let mut file = open_file(&path)?;
 
     // Headers
-    write!(file, "index{delim}data\n")?;
+    write!(file, "index{delim}id{delim}name{delim}email{delim}city\n")?;
 
     // Data
     for i in 1..=lines {
-        write!(file, "{i}{delim}data\n")?;
+        let name: String = Name().fake();
+        let email: String = FreeEmail().fake();
+        let city: String = CityName().fake();
+        let id: String = UUIDv4.fake();
+
+        write!(
+            file,
+            "{i}{delim}{id}{delim}{name}{delim}{email}{delim}{city}\n"
+        )?;
     }
 
     Ok(())
