0a21d7e884765a8d5d4a513fde158b343a8cf704
Lewis Hendy <lewishendy26@gmail.com>
2026-06-06 13:33:57 +0100

    expanded column type options

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use clap::{Parser, Subcommand};
 use fake::Fake;
 use fake::faker::address::en::*;
+use fake::faker::creditcard::en::CreditCardNumber;
+use fake::faker::filesystem::ar_sa::FileExtension;
+use fake::faker::filesystem::en::FileName;
+use fake::faker::filesystem::en::FilePath;
+use fake::faker::http::en::RfcStatusCode;
 use fake::faker::internet::en::*;
+use fake::faker::lorem::en::Word;
 use fake::faker::name::en::*;
+use fake::faker::phone_number::ar_sa::PhoneNumber;
+use fake::faker::time::ar_sa::Date;
+use fake::faker::time::ar_sa::DateTime;
+use fake::faker::time::ar_sa::Time;
 use fake::uuid::UUIDv4;
 use std::fs::File;
 use std::fs::OpenOptions;
@@ -31,7 +41,7 @@ enum Commands {
         #[arg(
             short,
             long,
-            default_value = "index:index, id:uuid, name:full_name, email:email",
+            default_value = "index:index, id:uuid, title:title, first_name:first_name, last_name:last_name, email:email, post_code:post_code, street_name:street_name, building_number:building_number, file_path:file_path",
             value_delimiter = ','
         )]
         columns: Vec<String>,
@@ -46,6 +56,7 @@ enum Commands {
     },
 }
 
+// Opens file and returns file handle
 fn open_file(path: &str) -> Result<File, io::Error> {
     OpenOptions::new()
         .write(true)
@@ -54,6 +65,7 @@ fn open_file(path: &str) -> Result<File, io::Error> {
         .open(path)
 }
 
+// Generates and writes csv file from input params
 fn write_csv(path: &str, lines: u32, delim: String, columns: Vec<String>) -> Result<(), io::Error> {
     let mut file = open_file(&path)?;
     let mut headers: Vec<String> = Vec::new();
@@ -76,9 +88,41 @@ fn write_csv(path: &str, lines: u32, delim: String, columns: Vec<String>) -> Res
             let col = match t.as_str() {
                 "index" => i.to_string(),
                 "uuid" => UUIDv4.fake(),
-                "email" => FreeEmail().fake(),
-                "city" => CityName().fake(),
+                "char" => {
+                    let w: String = Word().fake();
+                    w.chars().next().unwrap_or('a').to_string()
+                }
+
+                "post_code" => PostCode().fake(),
+                "zip_code" => ZipCode().fake(),
+                "country_name" => CountryName().fake(),
+                "city_name" => CityName().fake(),
+                "street_name" => StreetName().fake(),
+                "building_number" => BuildingNumber().fake(),
+
+                "title" => Title().fake(),
                 "full_name" => Name().fake(),
+                "first_name" => FirstName().fake(),
+                "last_name" => LastName().fake(),
+                "username" => Username().fake(),
+                "password" => Password(8..20).fake(),
+
+                "ip" => IPv4().fake(),
+                "email" => SafeEmail().fake(),
+                "phone" => PhoneNumber().fake(),
+                "credit_card_number" => CreditCardNumber().fake(),
+
+                "date" => Date().fake(),
+                "time" => Time().fake(),
+                "date_time" => DateTime().fake(),
+
+                "file_path" => FilePath().fake(),
+                "file_name" => FileName().fake(),
+                "file_ext" => FileExtension().fake(),
+
+                "http_status_code" => RfcStatusCode().fake(),
+                "rfc_status_code" => RfcStatusCode().fake(),
+
                 _ => String::new(),
             };
 
@@ -95,6 +139,7 @@ fn write_csv(path: &str, lines: u32, delim: String, columns: Vec<String>) -> Res
     Ok(())
 }
 
+// Parse cli params and
 fn main() {
     let cli = Cli::parse();
 
@@ -105,11 +150,11 @@ fn main() {
             delim,
             columns,
         } => match write_csv(&path, lines, delim, columns) {
-            Ok(_file) => println!("Opened successfully"),
+            Ok(_file) => println!("CSV File generated"),
             Err(e) => println!("Failed: {e}"),
         },
         Commands::Xml { path, lines: _ } => match open_file(&path) {
-            Ok(_file) => println!("Opened successfully"),
+            Ok(_file) => println!("XML File generated"),
             Err(e) => println!("Failed: {e}"),
         },
     }
